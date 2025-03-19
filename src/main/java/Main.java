3c739af5721fece52f19cc3e31501c7f811b35f3
RadoslawSzczepanski <radoslaw.szczepanski327@gmail.com>
2025-03-19 18:03:31 +0000

    2_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Celsjusz\tFahrenheit");
-    System.out.println("-------------------");
+    System.out.println("Celsjusz Fahrenheit");
+    System.out.println(" ");
     
     for (int celsius = 0; celsius <= 300; celsius += 20) {
       double fahrenheit = (celsius * 9.0/5.0) + 32;
